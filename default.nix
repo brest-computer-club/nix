7ab2d8b6b6ef21649c4c657dabe6ab78c88033e8
brest computer club <brest-computer-club@protonmail.com>
2020-11-26 13:39:41 +0100

    fix kubectl path

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@ nixPkgs:
 {
   terraform_0_13_2 = import ./pkgs/terraform-0.13.2.nix nixPkgs;
   elm_0_19_1 = import ./pkgs/elm-0.19.1.nix nixPkgs;
-  kubectl_1_19_4 = import ./pkgs/kubectl nixPkgs "1.19.4";
+  kubectl_1_19_4 = import ./pkgs/kubectl.nix nixPkgs "1.19.4";
 
   bundles = {
     elm = import ./bundles/elm.nix;
